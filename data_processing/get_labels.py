194b30566b00f7b13cd649911d038b3a75d77f18
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-23 14:00:23 +0100

    only save index

--- data_processing/get_labels.py
+++ data_processing/get_labels.py
@@ -10,6 +10,6 @@ import os
 path = 'norsentlex/Fullform'
 for file in os.listdir(path):
     if 'Positive.json' in file:
-        pos = pd.read_json(str(path+'/'+file))
-        
+        pos = pd.read_json(str(path+'/'+file), orient='index')
+print(pos)        
 # Negative
