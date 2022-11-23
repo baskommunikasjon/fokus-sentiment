a0d5ca2b78dabef37a47757113bd8f850a8b0fce
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-23 13:57:42 +0100

    no need for list

--- data_processing/get_labels.py
+++ data_processing/get_labels.py
@@ -3,5 +3,13 @@
 # Import packages
 import numpy as np
 import pandas as pd
+import os
 
-# 
+# Positive
+
+path = 'norsentlex/Fullform'
+for file in os.listdir(path):
+    if 'Positive.json' in file:
+        pos = pd.read_json(str(path+'/'+file))
+        
+# Negative
