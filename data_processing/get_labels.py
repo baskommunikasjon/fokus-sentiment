165f0d6f8e60dee510e047542fdc7664b8c1d5ef
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-24 10:51:46 +0100

    add elif to this

--- data_processing/get_labels.py
+++ data_processing/get_labels.py
@@ -14,13 +14,14 @@ for file in os.listdir(path):
     if 'Positive.json' in file:
         pos = pd.read_json(str(path+'/'+file), orient='index')
         pos_explode_fixed = pos.apply(pd.Series.explode).reset_index()
+        # Save result as an excel to check if explode worked
+        pos_explode_fixed.rename(columns={'index':'Word'}, inplace=True)
+        print(pos_explode_fixed)  
+        pos_explode_fixed.to_excel('data/Positive words.xlsx')
+    elif 'Negative.json' in file:
+        neg = pd.read_json(str(path+'/'+file), orient='index')
+        neg_explode_fixed = neg.apply(pd.Series.explode).reset_index()
+        neg_explode_fixed.rename(columns={'index':'Word'}, inplace=True)
+        print(neg_explode_fixed)  
+        neg_explode_fixed.to_excel('data/Negative words.xlsx')
         
-        
-
-print(pos)   
-print(pos_explode_fixed)   
-#print(fixed_dataframe) 
-#print(fixed_dataframe)
-# Save result as an excel to check if explode worked
-pos_explode_fixed.to_excel('data/Positive words.xlsx')
-# Negative
