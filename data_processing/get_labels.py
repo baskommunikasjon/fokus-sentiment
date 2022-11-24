69aa3ad8ed64d0633b44ce6d820a95b053f48dec
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-24 10:44:31 +0100

    simple fix with just one apply

--- data_processing/get_labels.py
+++ data_processing/get_labels.py
@@ -6,29 +6,21 @@ import pandas as pd
 import os
 import json
 from ast import literal_eval
+import openpyxl
 # Positive
 
 path = 'norsentlex/Fullform'
 for file in os.listdir(path):
     if 'Positive.json' in file:
         pos = pd.read_json(str(path+'/'+file), orient='index')
-        #pos = json.loads(file) 
-        '''- it does not work. This contains
-         an error '''
-        pos_explode = pos['forms'].apply(pd.Series)
-        fixed_columns = []
-        for col in pos_explode.columns:
-            #pos_explode_fixed = pos_explode[col].apply(pd.Series)
-            pos_explode_fixed = pos_explode[col].explode()
-            fixed_columns.append(pos_explode_fixed)
-        fixed_dataframe = pd.concat(fixed_columns)
-        #fixed_dataframe = pd.concat(fixed_columns)
+        pos_explode_fixed = pos.apply(pd.Series.explode).reset_index()
+        
         
 
 print(pos)   
-print(pos_explode)   
-print(fixed_dataframe) 
+print(pos_explode_fixed)   
+#print(fixed_dataframe) 
 #print(fixed_dataframe)
 # Save result as an excel to check if explode worked
-
+pos_explode_fixed.to_excel('data/Positive words.xlsx')
 # Negative
