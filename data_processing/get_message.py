1085e13ca857a5ca3ce417e846bfb5b8fa4a1cbb
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-24 15:21:57 +0100

    add result

--- data_processing/get_message.py
+++ data_processing/get_message.py
@@ -2,15 +2,22 @@
 
 def get_sentiment(text_results, pos, neg):
     print(text_results)
+
     for token in text_results:
         #print(token.text, token.pos_, token.dep_, token.tag_, token.morph)
+        positive_feeling = []
+        negative_feeling =[]
         if token.text in pos['Word'].values:
             print(f'{token.text} is positive: and contains a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
             f'the morphology is {token.morph}')
-            print('The sentence contains a Positive feeling')
+            positive_feeling.append(token)
         elif token.text in neg['Word'].values:
             print(f'{token.text} is negative: and contains a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
             f'the morphology is {token.morph}')
-            print('The sentence contains a Negative feeling')
+            negative_feeling.append(token)
         else:
             continue     
+        if len(positive_feeling) > len(negative_feeling):
+            print('The sentence contains a Positive feeling')
+        elif len(positive_feeling) < len(negative_feeling):
+            print('The sentence contains a Negative feeling')
