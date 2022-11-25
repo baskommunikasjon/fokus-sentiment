4a561b8f31c03781fe589e0e7ca6468a27b341ed
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-25 11:28:06 +0100

    add indentation and fix readme

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 This is the playground where we train bas norwegian NLP algorithm
 that will recognize `positive`, `negative`, `ironical` and
-`sarcastic` sentiment. 
+`sarcastic` sentiment. Also add the other explanations
 
 # CORPUS
 
 We are going to use the Negative and Positive words to loop through sentences and determine
 positive and negative. Also, we will use possesive words and gender to understand further.
-We need more positive and negative nouns, pronouns and modifiers
+We need more positive and negative nouns, pronouns and modifiers.
+
 # PRETRAINING
 
 Will be done using Keras in TensorFlow. Waiting for full scraping of blogs and texts to get a 
--- data_processing/get_message.py
+++ data_processing/get_message.py
@@ -3,19 +3,21 @@
 def get_sentiment(text_results, pos, neg):
     print(text_results)
     positive_feeling = []
-    negative_feeling =[]
+    negative_feeling = []
     for token in text_results:
         #print(token.text, token.pos_, token.dep_, token.tag_, token.morph)
         if token.text in pos['Word'].values:
-            print(f'{token.text} is positive: and contains a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
-            f'the morphology is {token.morph}')
+            print(f'{token.text} is positive: and contains'
+                  ' a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
+                  f'the morphology is {token.morph}')
             positive_feeling.append(token)
         elif token.text in neg['Word'].values:
-            print(f'{token.text} is negative: and contains a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
-            f'the morphology is {token.morph}')
+            print(f'{token.text} is negative: and contains'
+                  ' a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
+                  f'the morphology is {token.morph}')
             negative_feeling.append(token)
         else:
-            continue     
+            continue
     if len(positive_feeling) > len(negative_feeling):
         print('The sentence contains a Positive feeling')
     elif len(positive_feeling) < len(negative_feeling):
