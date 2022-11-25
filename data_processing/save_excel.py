de5c951477b00bc367a9d29eb4789df1bae0594d
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-25 12:10:59 +0100

    add try except in case writer does not work

--- data_processing/save_excel.py
+++ data_processing/save_excel.py
@@ -2,12 +2,18 @@
 
 # Import packages
 import pandas as pd
+import xlsxwriter
 
 # Functions
 def save_excel(df1, df2, sheetname_1, sheetname_2):
     writer = pd.ExcelWriter('data/.xlsx', 
                             engine ='xlsxwriter')
-    
-    df1.to_excel(writer, sheet_name=sheetname_1)
-    df2.to_excel(writer, sheet_name=sheetname_2)
+    try:
+        df1.to_excel(writer, sheet_name=sheetname_1)
+    except Exception:
+        pass
+    try:
+        df2.to_excel(writer, sheet_name=sheetname_2)
+    except Exception:
+        pass
     writer.close()
