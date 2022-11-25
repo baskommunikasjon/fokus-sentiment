9d90b321ced4bb8bd256374c44b9238e1324ae11
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-25 11:56:56 +0100

    add dataframe per individual level

--- data_processing/get_message.py
+++ data_processing/get_message.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+# Import packages
+import pandas as pd
+
+# Functions
+
+
 def get_sentiment(text_results, pos, neg):
     print(text_results)
     positive_feeling = []
@@ -8,14 +14,26 @@ def get_sentiment(text_results, pos, neg):
         #print(token.text, token.pos_, token.dep_, token.tag_, token.morph)
         if token.text in pos['Word'].values:
             print(f'{token.text} is positive: and contains'
-                  ' a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
+                  f' a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
                   f'the morphology is {token.morph}')
-            positive_feeling.append(token)
+            df_token = pd.DataFrame(
+                {'Word': token.text,
+                 'Part of speech tag - coarsed grained': token.pos_,
+                 'Dependency label': token.dep_,
+                 'Part og speech tag - fine grained': token.tag_,
+                 'Morphology': token.morph})
+            positive_feeling.append(df_token)
         elif token.text in neg['Word'].values:
             print(f'{token.text} is negative: and contains'
-                  ' a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
+                  f' a {token.pos_, token.dep_} tagged as {token.tag_}, and \n'
                   f'the morphology is {token.morph}')
-            negative_feeling.append(token)
+            df_token = pd.DataFrame(
+                {'Word': token.text,
+                 'Part of speech tag - coarsed grained': token.pos_,
+                 'Dependency label': token.dep_,
+                 'Part og speech tag - fine grained': token.tag_,
+                 'Morphology': token.morph})
+            negative_feeling.append(df_token)
         else:
             continue
     if len(positive_feeling) > len(negative_feeling):
@@ -23,4 +41,4 @@ def get_sentiment(text_results, pos, neg):
     elif len(positive_feeling) < len(negative_feeling):
         print('The sentence contains a Negative feeling')
     else:
-        'Job done'
+        'Recognition done'
