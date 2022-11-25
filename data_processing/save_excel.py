b2b3c337dec1a0a75851532046d848c4f19dd628
mariaangelesjs <mariaangelesjs@gmail.com>
2022-11-25 13:59:50 +0100

    add loop

--- data_processing/save_excel.py
+++ data_processing/save_excel.py
@@ -8,12 +8,15 @@ import xlsxwriter
 def save_excel(df1, df2,filename,sheetname_1, sheetname_2):
     writer = pd.ExcelWriter(f'data/{filename}.xlsx', 
                             engine ='xlsxwriter')
-    try:
-        df1.to_excel(writer, sheet_name=sheetname_1)
-    except Exception:
-        pass
-    try:
-        df2.to_excel(writer, sheet_name=sheetname_2)
-    except Exception:
+    if len(df1)>1:
+        try:
+            df1.to_excel(writer, sheet_name=sheetname_1)
+        except Exception:
+            pass
+        try:
+            df2.to_excel(writer, sheet_name=sheetname_2)
+        except Exception:
+            pass
+    elif len(df1)==0:
         pass
     writer.close()
